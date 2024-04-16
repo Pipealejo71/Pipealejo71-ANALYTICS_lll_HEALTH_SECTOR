3aee69e6fa55e5bd8c00086de3d0cd050a1efc74
Pipealejo71 <cristhian.alejo@udea.edu.co>
2024-04-16 16:01:04 -0500

    preprocesamiento

--- Exploracion.py
+++ Exploracion.py
@@ -18,3 +18,29 @@ egresos.shape
 usuario.info()
 usuario.shape
 
+#Preprocesamiento de datos
+## Usuario
+usuario['YEAR'] = usuario['YEAR'].astype(str)
+usuario['NRODOC'] = usuario['NRODOC'].astype(str)
+
+usuario['DEPARTAMENTO'].value_counts()
+usuario = usuario.drop('DEPARTAMENTO', axis=1)
+
+usuario['PRIMERA CLASE FUNCIONAL'] = usuario['PRIMERA CLASE FUNCIONAL'].str.upper()
+usuario['ÚLTIMA CLASE FUNCIONAL'] = usuario['PRIMERA CLASE FUNCIONAL'].str.upper()
+usuario['PRIMERA CLASE FUNCIONAL'].value_counts()
+usuario['ÚLTIMA CLASE FUNCIONAL'].value_counts()
+
+usuario['SEXO'].value_counts()
+usuario = usuario[usuario['SEXO'].isin(['F', 'M'])]
+usuario['SEXO'].value_counts()
+
+## Egresos
+egresos['YEAR'] = egresos['YEAR'].astype(str)
+egresos['NRODOC'] = egresos['NRODOC'].astype(str)
+egresos['NRO ATENCION'] = egresos['NRO ATENCION'].astype(str)
+egresos['NRO INGRESO'] = egresos['NRO INGRESO'].astype(str)
+egresos['SERVICIO HABILITADO COD'] = egresos['SERVICIO HABILITADO COD'].astype(str)
+
+egresos['PERTINENCIA DIAGNOSTICA'].value_counts()
+egresos = egresos.drop('PERTINENCIA DIAGNOSTICA', axis=1)
