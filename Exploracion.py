fe14a60786e7ea4cb2964be3450b944648ea05aa
Javier Elohim Burgos Chaguezac <ing.burgoschaguezac@gmail.com>
2024-04-16 19:16:02 -0500

    base_cronicos

--- Exploracion.py
+++ Exploracion.py
@@ -6,35 +6,42 @@ import sys
 
 ## Ruta directorio qué tiene paquetes
 sys.path
-sys.path.append('C:\\Users\\User\\Desktop\\Analitica lll\\Pipealejo71-ANALYTICS_lll_HEALTH_SECTOR') ## este comanda agrega una ruta
+#sys.path.append('C:\\Users\\User\\Desktop\\Analitica lll\\Pipealejo71-ANALYTICS_lll_HEALTH_SECTOR') ## este comanda agrega una ruta
+sys.path.append('C:\\Users\\JavierBurgos\\Desktop\\LOCAL\\Analítica 3\\UNIDADES\\Unidad 3 - Aplicaciones en Operaciones (Salud)\\5.1 Entrega\\Pipealejo71-ANALYTICS_lll_HEALTH_SECTOR')
+
 #Carga de datos
-egresos = ('RETO_df_egresos.csv')
-usuario = ('RETO_df_usuarios.csv')
-egresos=pd.read_csv(egresos)
-usuario=pd.read_csv(usuario)
+#egresos = ('RETO_df_egresos.csv')
+#usuarios = ('RETO_df_usuarios.csv')
+#cronicos = ('RETO_df_cronicos.csv')
+
+egresos=pd.read_csv('RETO_df_egresos.csv')
+usuarios=pd.read_csv('RETO_df_usuarios.csv')
+cronicos=pd.read_csv('RETO_df_cronicos.csv')
 
 #Exploración de datos
 egresos.info()
 egresos.shape
-usuario.info()
-usuario.shape
+usuarios.info()
+usuarios.shape
+cronicos.info()
+cronicos.shape
 
 #Preprocesamiento de datos
 ## Usuario
-usuario['YEAR'] = usuario['YEAR'].astype(str)
-usuario['NRODOC'] = usuario['NRODOC'].astype(str)
+usuarios['YEAR'] = usuarios['YEAR'].astype(str)
+usuarios['NRODOC'] = usuarios['NRODOC'].astype(str)
 
-usuario['DEPARTAMENTO'].value_counts()
-usuario = usuario.drop('DEPARTAMENTO', axis=1)
+usuarios['DEPARTAMENTO'].value_counts()
+usuarios = usuarios.drop('DEPARTAMENTO', axis=1)
 
-usuario['PRIMERA CLASE FUNCIONAL'] = usuario['PRIMERA CLASE FUNCIONAL'].str.upper()
-usuario['ÚLTIMA CLASE FUNCIONAL'] = usuario['PRIMERA CLASE FUNCIONAL'].str.upper()
-usuario['PRIMERA CLASE FUNCIONAL'].value_counts()
-usuario['ÚLTIMA CLASE FUNCIONAL'].value_counts()
+usuarios['PRIMERA CLASE FUNCIONAL'] = usuarios['PRIMERA CLASE FUNCIONAL'].str.upper()
+usuarios['ÚLTIMA CLASE FUNCIONAL'] = usuarios['PRIMERA CLASE FUNCIONAL'].str.upper()
+usuarios['PRIMERA CLASE FUNCIONAL'].value_counts()
+usuarios['ÚLTIMA CLASE FUNCIONAL'].value_counts()
 
-usuario['SEXO'].value_counts()
-usuario = usuario[usuario['SEXO'].isin(['F', 'M'])]
-usuario['SEXO'].value_counts()
+usuarios['SEXO'].value_counts()
+usuarios = usuarios[usuarios['SEXO'].isin(['F', 'M'])]
+usuarios['SEXO'].value_counts()
 
 ## Egresos
 egresos['YEAR'] = egresos['YEAR'].astype(str)
